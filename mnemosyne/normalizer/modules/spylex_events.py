b5918d27264551aa0ac2cde64351c1b210c0bf63
amerck <alexander.t.merck@gmail.com>
2020-07-07 19:40:21 -0400

    Adding new honeypot types

---
+++ mnemosyne/normalizer/modules/spylex_events.py
@@ -0,0 +1,25 @@
+from normalizer.modules.basenormalizer import BaseNormalizer
+import logging
+
+
+class SpylexEvents(BaseNormalizer):
+    channels = ('spylex.events',)
+
+    def normalize(self, data, channel, submission_timestamp, ignore_rfc1918=True):
+        o_data = self.parse_record_data(data)
+
+        if ignore_rfc1918 and self.is_RFC1918_addr(o_data['src_ip']):
+            return []
+
+        session = {
+            'timestamp': submission_timestamp,
+            'source_ip': o_data['src_ip'],
+            'source_port': o_data['src_port'],
+            'destination_port': o_data['dst_port'],
+            'honeypot': 'spylex',
+            'protocol': 'http',
+        }
+
+        relations = [{'session': session}, ]
+
+        return relations
