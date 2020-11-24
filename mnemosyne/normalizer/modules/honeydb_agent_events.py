22ace59f6fcd582d91596d81a7e8cbe45acdc348
Jesse Bowling <jesse.bowling@duke.edu>
2020-11-23 20:35:39 -0500

    try/catch and logging
    Signed-off-by: Jesse Bowling <jesse.bowling@duke.edu>

--- mnemosyne/normalizer/modules/honeydb_agent_events.py
+++ mnemosyne/normalizer/modules/honeydb_agent_events.py
@@ -1,5 +1,13 @@
 from normalizer.modules.basenormalizer import BaseNormalizer
+import logging
 
+LOG_FORMAT = '%(asctime)s - %(levelname)s - %(name)s[%(lineno)s][%(filename)s] - %(message)s'
+
+handler = logging.StreamHandler()
+handler.setFormatter(logging.Formatter(LOG_FORMAT))
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+logger.addHandler(handler)
 
 class HoneydbAgentEvents(BaseNormalizer):
     channels = ('honeydb-agent.events',)
@@ -8,18 +16,20 @@ class HoneydbAgentEvents(BaseNormalizer):
         o_data = self.parse_record_data(data)
 
         if ignore_rfc1918 and self.is_RFC1918_addr(o_data['peerIP']):
+            logger.warning('Ignoring RFC1918 address per configuration: {}'.format(o_data['peerIP']))
             return []
-
-        session = {
-            'timestamp': submission_timestamp,
-            'source_ip': o_data['remote_host'],
-            'source_port': o_data['remote_port'],
-            'destination_ip': o_data['local_host'],
-            'destination_port': o_data['local_port'],
-            'honeypot': 'honeydb-agent',
-            'protocol': o_data['service']
-        }
-
+        try:
+            session = {
+                'timestamp': submission_timestamp,
+                'source_ip': o_data['remote_host'],
+                'source_port': o_data['remote_port'],
+                'destination_ip': o_data['local_host'],
+                'destination_port': o_data['local_port'],
+                'honeypot': 'honeydb-agent',
+                'protocol': o_data['service']
+            }
+        except Exception as e:
+            logger.error('Error constructing sesssion in Analyzer {}: {}'.format(self.__name__, e))
         relations = [{'session': session}, ]
 
         return relations
